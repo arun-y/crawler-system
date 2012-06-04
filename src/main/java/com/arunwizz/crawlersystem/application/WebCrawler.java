7a91d20aca2c1b32ab49b081199b7376bfdb8a2f
Arun Yadav <arunwizz@gmail.com>
2012-06-05 00:14:11 +0530

    Started to build Craler Appliation part of system

--- src/main/java/com/arunwizz/crawlersystem/application/WebCrawler.java
+++ src/main/java/com/arunwizz/crawlersystem/application/WebCrawler.java
@@ -1,5 +1,8 @@
 package com.arunwizz.crawlersystem.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main class to be called to begin web crawling
  * 
@@ -10,7 +13,14 @@ package com.arunwizz.crawlersystem.application;
  */
 public class WebCrawler {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WebCrawler.class);
+	
 	public static void main(String argv[]) {
+		if (argv.length != 1) {
+			LOGGER.error("Usage: {}", "WebClawler seed-file-location");
+			System.exit(1);
+		}
+		String seedFile = argv[0];
 		
 		
 		
